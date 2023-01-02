6c55e2fc38b74bb6e553bc238982b6984e7e1637
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-01-02 23:07:50 +0530

    feat: unit testing

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "tom",
+		Balance:  50,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+}
