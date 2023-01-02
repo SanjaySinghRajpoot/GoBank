4097f80068668b9c45080a4381b6bb4556de86bb
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-01-02 22:17:08 +0530

    feat: create, update and delete sqlc

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,18 @@ INSERT INTO accounts (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE accounts
+SET balance = $2
+WHERE id = $1;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SanjaySinghRajpoot/GoBank
+
+go 1.18
