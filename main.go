863b5181cf7fa00871774a74fe23f4e746898b11
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-01-06 23:01:37 +0530

    feat: .env added

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/SanjaySinghRajpoot/GoBank/api"
 	db "github.com/SanjaySinghRajpoot/GoBank/db/sqlc"
+	"github.com/SanjaySinghRajpoot/GoBank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
 )
 
 func main() {
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config")
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to the database", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
