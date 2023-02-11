a8f03697dd6aec948c2ffa44f20843989bd93c91
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-02-11 23:03:01 +0530

    db migration

--- db/query/user.sql
+++ db/query/user.sql
@@ -12,3 +12,22 @@ RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users
 WHERE username = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET
+  hashed_password = CASE
+    WHEN @set_hashed_password::boolean = TRUE THEN @hashed_password
+    ELSE hashed_password
+  END,
+    full_name = CASE
+    WHEN @set_full_name = TRUE THEN @full_name
+    ELSE full_name
+  END,
+  email = CASE
+    WHEN @set_email = TRUE THEN @email
+    ELSE email
+  END
+WHERE
+  username = @username
+RETURNING *;
--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -65,3 +65,55 @@ func (q *Queries) GetUser(ctx context.Context, username string) (User, error) {
 	)
 	return i, err
 }
+
+const updateUser = `-- name: UpdateUser :one
+UPDATE users
+SET
+  hashed_password = CASE
+    WHEN $1::boolean = TRUE THEN $2
+    ELSE hashed_password
+  END,
+    full_name = CASE
+    WHEN $3 = TRUE THEN $4
+    ELSE full_name
+  END,
+  email = CASE
+    WHEN $5 = TRUE THEN $6
+    ELSE email
+  END
+WHERE
+  username = $7
+RETURNING username, hashed_password, full_name, email, password_changed_at, created_at
+`
+
+type UpdateUserParams struct {
+	SetHashedPassword bool        `json:"set_hashed_password"`
+	HashedPassword    string      `json:"hashed_password"`
+	SetFullName       interface{} `json:"set_full_name"`
+	FullName          string      `json:"full_name"`
+	SetEmail          interface{} `json:"set_email"`
+	Email             string      `json:"email"`
+	Username          string      `json:"username"`
+}
+
+func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, updateUser,
+		arg.SetHashedPassword,
+		arg.HashedPassword,
+		arg.SetFullName,
+		arg.FullName,
+		arg.SetEmail,
+		arg.Email,
+		arg.Username,
+	)
+	var i User
+	err := row.Scan(
+		&i.Username,
+		&i.HashedPassword,
+		&i.FullName,
+		&i.Email,
+		&i.PasswordChangedAt,
+		&i.CreatedAt,
+	)
+	return i, err
+}
